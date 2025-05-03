b80e8ee383815c94534472aedf6cd4c6654caf15
DaHeeO <134845297+DaHeeO@users.noreply.github.com>
2025-05-03 18:51:16 +0900

    .Font setting, Layout modified

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import SideBar from './SideBar';
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
+import Header from './Header';
 
 function Layout() {
     return (
-        <div className={styles.wrapper}>
-            <SideBar />
-            <main>
-                <Outlet />
-            </main>
-        </div>
+        <>
+            <Header />
+            <div className={styles.wrapper}>
+                <SideBar />
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </>
     );
 }
 
--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './SideBar.module.css';
+import logo from '../../assets/logo.svg';
 
 function SideBar() {
     return (
-        <div>
-            <p>SideBar Section</p>
+        <div className={styles.wrapper}>
+            <header className={styles.headerContainer}>
+                <img src={logo} alt="logo" />
+                <h1>tasky</h1>
+            </header>
         </div>
     );
 }
